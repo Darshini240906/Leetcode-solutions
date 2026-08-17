25c6437d60bd303b5a98209fe466875ae2c959d1
Darshini240906 <darshini240906@gmail.com>
2026-08-17 20:17:41 +0530

    Time: 16 ms (11.94%), Space: 18.4 MB (35.05%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    int countLessEqual(vector<vector<int>>& matrix, int mid) {
-        int n = matrix.size();
-        int row = n - 1, col = 0;
-        int count = 0;
-
-        while (row >= 0 && col < n) {
-            if (matrix[row][col] <= mid) {
-                count += row + 1;
-                col++;
-            } else {
-                row--;
-            }
-        }
-
-        return count;
-    }
-
     int kthSmallest(vector<vector<int>>& matrix, int k) {
         int n = matrix.size();
 
-        int left = matrix[0][0];
-        int right = matrix[n - 1][n - 1];
+        using T = tuple<int,int,int>;
+        priority_queue<T, vector<T>, greater<T>> minHeap;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (countLessEqual(matrix, mid) < k)
-                left = mid + 1;
-            else
-                right = mid;
+        for (int i = 0; i < n; i++) {
+            minHeap.push({matrix[i][0], i, 0});
         }
 
-        return left;
+        int val = 0;
+
+        for (int count = 0; count < k; count++) {
+            auto [v, r, c] = minHeap.top();
+            minHeap.pop();
+
+            val = v;
+
+            if (c + 1 < n) {
+                minHeap.push({matrix[r][c+1], r, c+1});
+            }
+        }
+
+        return val;
     }
 };
