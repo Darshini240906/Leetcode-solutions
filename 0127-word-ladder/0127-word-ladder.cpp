dc08d338d6f8e8bffe635f019c0da9b37a59b247
Darshini240906 <darshini240906@gmail.com>
2026-09-19 11:15:01 +0530

    Time: 31 ms (97.61%), Space: 19.6 MB (94.76%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        if (!dict.count(endWord)) return 0; 
+
+        unordered_set<string> beginSet{beginWord}, endSet{endWord};
+        int steps = 1;
+
+        while (!beginSet.empty() && !endSet.empty()) {
+            // Always expand the smaller frontier
+            if (beginSet.size() > endSet.size()) swap(beginSet, endSet);
+
+            unordered_set<string> next;
+            for (string word : beginSet) {
+                for (int i = 0; i < word.size(); i++) {
+                    char original = word[i];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        word[i] = c;
+                        if (endSet.count(word)) return steps + 1;
+                        if (dict.count(word)) {
+                            next.insert(word);
+                            dict.erase(word);
+                        }
+                    }
+                    word[i] = original;
+                }
+            }
+            beginSet = next;
+            steps++;
+        }
+        return 0;
+    }
+};
